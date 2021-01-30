01756527a56936cccf1b07e111b7248b5ddf1a38
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-01-30 17:24:18 +0800

    Create index.js

---
+++ sso-website/src/main/webapp/source/adduser/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AddUserPage from './AddUserPage'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <AddUserPage />
+    </React.StrictMode>,
+    document.getElementById('addUserRoot')
+);
