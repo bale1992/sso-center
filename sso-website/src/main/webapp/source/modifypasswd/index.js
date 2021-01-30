7d8e3768165a55e3c22973237a636e49c49a6098
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-01-30 17:24:53 +0800

    Create index.js

---
+++ sso-website/src/main/webapp/source/modifypasswd/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ModifyPasswdPage from './ModifyPasswdPage'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ModifyPasswdPage />
+    </React.StrictMode>,
+    document.getElementById('modifyPasswdRoot')
+);
