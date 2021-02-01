a69ee62b3e0243b1a10cf4e5afc3edd5628954e3
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-01 15:22:03 +0800

    Create RestBeanPostProcessor.java

---
+++ sso-service/src/main/common/com/demo/resourcepool/RestBeanPostProcessor.java
@@ -0,0 +1,25 @@
+package com.demo.resourcepool;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RestBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        log.warn("Current init bean, beanName:{}, beanClassName:{}", beanName, bean.getClass().getName());
+        if (ResourcePool.isController(bean)) {
+            ResourcePool.addRestController(bean);
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+}
