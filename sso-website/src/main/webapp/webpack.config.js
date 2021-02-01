e7af445731970f5bfbdbc16cd4f06f430e2e10ad
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-01 10:25:43 +0800

    Update webpack.config.js

--- sso-website/src/main/webapp/webpack.config.js
+++ sso-website/src/main/webapp/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -32,5 +34,26 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/source/index.html'),
+            filename: 'AddUserPage.html',
+            title: '创建用户',
+            chunks: ['addUserPage']
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/source/index.html'),
+            filename: 'LoginPage.html',
+            title: '登陆',
+            chunks: ['loginPage']
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/source/index.html'),
+            filename: 'ModifyPasswdPage.html',
+            title: '修改初始密码',
+            chunks: ['modifyPasswdPage']
+        }),
+        new CleanWebpackPlugin()
+    ],
     mode: 'development'
 };
