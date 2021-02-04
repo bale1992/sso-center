6c0158f2e3b18471735def711e1798e1ace0114c
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-04 16:07:06 +0800

    Update TblUserController.java

--- sso-service/src/main/java/com/demo/controller/TblUserController.java
+++ sso-service/src/main/java/com/demo/controller/TblUserController.java
@@ -2,9 +2,12 @@ package com.demo.controller;
 
 import com.demo.entity.TblUser;
 import com.demo.service.TblUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -15,6 +18,7 @@ import javax.annotation.Resource;
  * @author makejava
  * @since 2021-02-01 10:33:53
  */
+@Slf4j
 @RestController("/ssoservice/user")
 public class TblUserController {
     /**
@@ -27,10 +31,9 @@ public class TblUserController {
      * 新增数据
      *
      * @param tblUser 实例对象
-     * @return 实例对象
      */
     @PostMapping
-    public void addUser(TblUser tblUser) {
+    public void addUser(@RequestBody TblUser tblUser) {
         tblUserService.insert(tblUser);
     }
 
@@ -38,10 +41,9 @@ public class TblUserController {
      * 修改数据
      *
      * @param tblUser 实例对象
-     * @return 实例对象
      */
     @PutMapping
-    public void modifyUserPasswd(TblUser tblUser) {
+    public void modifyUserPasswd(@RequestBody TblUser tblUser) {
         tblUserService.update(tblUser);
     }
 
@@ -49,10 +51,9 @@ public class TblUserController {
      * 通过主键删除数据
      *
      * @param userName 主键
-     * @return 是否成功
      */
     @DeleteMapping
-    public void deleteUserByName(String userName) {
+    public void deleteUserByName(@RequestParam String userName) {
         tblUserService.deleteUser(userName);
     }
 }
