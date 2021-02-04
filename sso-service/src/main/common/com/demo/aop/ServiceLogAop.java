b88668e721b12987bac77b7c9addde2b65d1f006
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-04 16:05:52 +0800

    Create ServiceLogAop.java

---
+++ sso-service/src/main/common/com/demo/aop/ServiceLogAop.java
@@ -0,0 +1,33 @@
+package com.demo.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Aspect
+@Service
+public class ServiceLogAop {
+
+    @Pointcut("execution(public * com.demo.service.impl.*.*(*))")
+    public void pointcut() {
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) {
+        final Object[] args = joinPoint.getArgs();
+        log.info("{} invoked, input args:{}", joinPoint.getSignature().toShortString(), args);
+
+        Object proceed = null;
+        try {
+            proceed = joinPoint.proceed(args);
+        } catch (Throwable throwable) {
+            log.error("{} invoked exception:{}", joinPoint.getSignature().toShortString(), throwable.getMessage());
+        }
+        return proceed;
+    }
+
+}
