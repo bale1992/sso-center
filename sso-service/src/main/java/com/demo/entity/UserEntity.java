c443396e80f78aaa7895707098616ed94257e1b3
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-08 17:12:06 +0800

    Create UserEntity.java

---
+++ sso-service/src/main/java/com/demo/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * (TblUser)实体类
+ *
+ * @author makejava
+ * @since 2021-02-01 10:33:43
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class UserEntity implements Serializable {
+    private static final long serialVersionUID = 228590193511697547L;
+
+    private Long id;
+
+    private String userName;
+
+    private String passWord;
+
+    private Object role;
+
+    private boolean isLogin;
+
+    private boolean isAdminFirstLogin;
+
+}
