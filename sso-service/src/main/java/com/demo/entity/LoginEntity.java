5e952e2e59330f85bc9bf690977685b0921e5580
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-08 17:11:46 +0800

    Create LoginEntity.java

---
+++ sso-service/src/main/java/com/demo/entity/LoginEntity.java
@@ -0,0 +1,19 @@
+package com.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class LoginEntity {
+
+    private String userName;
+
+    private String passWord;
+}
