64e3abd312bfdf4cb72cab2caa26415c2b89906c
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-08 17:06:16 +0800

    Create ResourceMethodItem.java

---
+++ sso-service/src/main/common/com/demo/resourcepool/ResourceMethodItem.java
@@ -0,0 +1,22 @@
+package com.demo.resourcepool;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ResourceMethodItem {
+
+    private String methodClassName;
+
+    private String methodRestPath;
+
+}
