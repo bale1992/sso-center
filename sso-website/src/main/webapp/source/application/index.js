e7aae58537ebd78356f07ddb45fb0e6095bc2a30
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-02-18 15:17:46 +0800

    Create index.js

---
+++ sso-website/src/main/webapp/source/application/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppPages from "./AppPages";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <AppPages />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
