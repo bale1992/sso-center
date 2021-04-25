e1c52f0ebe28f7db6cb3de3f7e8e169c29852428
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-04-25 17:38:30 +0800

    Update KafkaClientUtil.java

--- sso-service/src/main/common/com/demo/util/KafkaClientUtil.java
+++ sso-service/src/main/common/com/demo/util/KafkaClientUtil.java
@@ -1,8 +1,11 @@
 package com.demo.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.IOException;
@@ -14,6 +17,7 @@ import java.util.Properties;
  */
 @Slf4j
 public final class KafkaClientUtil {
+    private static final String DEFAULT_SERVER = "127.0.0.1:9091,127.0.0.1:9092,127.0.0.1:9093";
     public static final String TOPIC_ONE = "topic_single_partition_three_replication";
     public static final String TOPIC_TWO = "topic_two_partition_three_replication";
     public static final String TOPIC_THREE = "topic_three_partition_two_replication";
@@ -22,7 +26,7 @@ public final class KafkaClientUtil {
     }
 
     public static KafkaProducer<String, String> createProducer() {
-        try (InputStream inputStream = InputStream.class.getResourceAsStream("/producer.properties")) {
+        try (InputStream inputStream = KafkaClientUtil.class.getClassLoader().getResourceAsStream("/kafka/producer.properties")) {
             final Properties properties = new Properties();
             properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getBootStrapServer());
             properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -35,7 +39,22 @@ public final class KafkaClientUtil {
         }
     }
 
+    public static KafkaConsumer<String, String> createConsumer(String groupName) {
+        try (InputStream inputStream = KafkaClientUtil.class.getClassLoader().getResourceAsStream("/kafka/consumer.properties")) {
+            final Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupName);
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, getBootStrapServer());
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.load(inputStream);
+            return new KafkaConsumer<>(properties);
+        } catch (IOException e) {
+            log.error("load kafka consumer config failed.");
+            return new KafkaConsumer<>(new Properties());
+        }
+    }
+
     private static String getBootStrapServer() {
-        return System.getenv("BOOTSTRAP_SERVER");
+        return System.getProperty("BOOTSTRAP_SERVER", DEFAULT_SERVER);
     }
 }
