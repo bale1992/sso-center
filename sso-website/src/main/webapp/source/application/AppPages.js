4c7d53c503ef96247bb6d90b3ff2a86a1f74279b
bale1992 <69375979+bale1992@users.noreply.github.com>
2021-04-25 17:42:25 +0800

    Update AppPages.js

--- sso-website/src/main/webapp/source/application/AppPages.js
+++ sso-website/src/main/webapp/source/application/AppPages.js
@@ -3,6 +3,16 @@ import axios from "axios";
 
 export default class AppPages extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            consumer1: '',
+            consumer2: '',
+            consumer3: '',
+        };
+    }
+
     oneOnClick() {
         axios.post('/rest/ssoservice/producer/one',
             null,
@@ -36,12 +46,95 @@ export default class AppPages extends Component {
         })
     }
 
+    userConsumer1OnChange(e) {
+        this.setState({consumer1: e.target.value});
+    }
+    consumerOneOnClick() {
+        axios.post('/rest/ssoservice/consumer/one',
+            {
+                consumerGroup: this.state.consumer1
+            },
+            {
+                'Content-Type': 'application/json'
+            }
+        ).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    userConsumer2OnChange(e) {
+        this.setState({consumer2: e.target.value});
+    }
+    consumerTwoOnClick() {
+        axios.post('/rest/ssoservice/consumer/two',
+            {
+                consumerGroup: this.state.consumer2
+            },
+            {
+                'Content-Type': 'application/json'
+            }
+        ).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    userConsumer3OnChange(e) {
+        this.setState({consumer3: e.target.value});
+    }
+    consumerThreeOnClick() {
+        axios.post('/rest/ssoservice/consumer/three',
+            {
+                consumerGroup: this.state.consumer3
+            },
+            {
+                'Content-Type': 'application/json'
+            }
+        ).catch(function (error) {
+            console.log(error);
+        })
+    }
+
     render() {
         return (
             <div>
-                <button onClick={()=>this.oneOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'ONE'}</button>
-                <button onClick={()=>this.twoOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'TWO'}</button>
-                <button onClick={()=>this.ThreeOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'THREE'}</button>
+                <div>
+                    <div>{'生产者'}</div>
+                    <button onClick={()=>this.oneOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'ONE'}</button>
+                    <button onClick={()=>this.twoOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'TWO'}</button>
+                    <button onClick={()=>this.ThreeOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'THREE'}</button>
+                </div>
+                <div>{'----------------------------------------------------------------------------------------------------------------------------------'}</div>
+                <div>
+                    <div>{'消费者'}</div>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <input value={this.state.consumer1} placeholder={'消费者组'} onChange={(e) => this.userConsumer1OnChange(e)}/>
+                            </td>
+                            <td>
+                                <button onClick={()=>this.consumerOneOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'ONE'}</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input value={this.state.consumer2} placeholder={'消费者组'} onChange={(e) => this.userConsumer2OnChange(e)}/>
+                            </td>
+                            <td>
+                                <button onClick={()=>this.consumerTwoOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'TWO'}</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input value={this.state.consumer3} placeholder={'消费者组'} onChange={(e) => this.userConsumer3OnChange(e)}/>
+                            </td>
+                            <td>
+                                <button onClick={()=>this.consumerThreeOnClick()} style={{ width: '100px', height: '30px', marginLeft: '60px' }}>{'THREE'}</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
